c0294835463f30a3cc5c42437786f34b9e0221a5
AlanArrietaF <arrietafloresalanalejandro@gmail.com>
2026-02-27 19:10:15 -0600

    Otra nueva tarea

---
+++ Tareas/tareaOpLogicas/app.js
@@ -0,0 +1,22 @@
+// Operadores Logicos
+/*
+&&: Si los dos son v nos da un V
+||: con que unos sea v dara v
+!: negacion
+*/
+
+var compara = true && true;
+console.log(compara);
+
+var compara = true || true;
+console.log(compara);
+
+
+var compara = false || true;
+console.log(compara);
+
+var compara = 5>2 && 6 !== 8;
+console.log(compara);
+ 
+
+
